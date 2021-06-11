3b7a928ca7cc20a34d99340cac6e10934d030c88
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2021-06-10 20:47:12 -0400

    Altera lógica do front(wip)

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,92 +1,41 @@
-const pieces = document.querySelectorAll('.piece');
-const squares = document.querySelectorAll('.square');
+const coordinates = [
+    { square: "A2", piece: "white-pawn" },
+    { square: "B2", piece: "white-pawn" },
+    { square: "C2", piece: "white-pawn" },
+    { square: "D2", piece: "white-pawn" },
+    { square: "E2", piece: "white-pawn" },
+    { square: "F2", piece: "white-pawn" },
+    { square: "G2", piece: "white-pawn" },
+    { square: "H2", piece: "white-pawn" },
+    { square: "A7", piece: "black-pawn" },
+    { square: "B7", piece: "black-pawn" },
+    { square: "C7", piece: "black-pawn" },
+    { square: "D7", piece: "black-pawn" },
+    { square: "E7", piece: "black-pawn" },
+    { square: "F7", piece: "black-pawn" },
+    { square: "G7", piece: "black-pawn" },
+    { square: "H7", piece: "black-pawn" },
+    { square: "A1", piece: "white-rook" },
+    { square: "B1", piece: "white-bishop" },
+    { square: "C1", piece: "white-knight" },
+    { square: "D1", piece: "white-queen" },
+    { square: "E1", piece: "white-king" },
+    { square: "F1", piece: "white-knight" },
+    { square: "G1", piece: "white-bishop" },
+    { square: "H1", piece: "white-rook" },
+    { square: "A8", piece: "black-rook" },
+    { square: "B8", piece: "black-bishop" },
+    { square: "C8", piece: "black-knight" },
+    { square: "D8", piece: "black-queen" },
+    { square: "E8", piece: "black-king" },
+    { square: "F8", piece: "black-knight" },
+    { square: "G8", piece: "black-bishop" },
+    { square: "H8", piece: "black-rook" },
+];
 
-const movement = {
-    piece: null,
-    square: null
-};
-
-const capture = {
-    piece1: null,
-    piece2: null
-};
-
-init();
-
-function init() {
-    addLogic();
-    saveNextMove();
-}
-
-function addLogic() {
-    for (const square of squares) {
-        square.removeEventListener('click', logic);
-        square.addEventListener('click', logic);
-    }
-}
-
-function saveNextMove() {
-    for (const piece of pieces) {
-        piece.addEventListener('click', () => {
-            movement.piece = piece;
-            if (capture.piece1 === null) {
-                capture.piece1 = piece;
-            } else if (piece !== capture.piece1) {
-                capture.piece2 = piece;
-            }
-        });
-    }
-}
-
-function cleanNextMove() {
-    movement.piece = null;
-    movement.square = null;
-
-    capture.piece1 = null;
-    capture.piece2 = null;
-
-    addLogic();
-}
-
-function promotion(piece) {
-    const isWhitePawn = piece.classList.contains('white-pawn');
-    const inRow8 = piece.parentElement.id.includes('8');
-    if (isWhitePawn && inRow8) {
-        piece.classList.remove('white-pawn');
-        piece.classList.add('white-queen');
-        return;
-    }
-
-    const isBlackPawn = piece.classList.contains('black-pawn');
-    const inRow1 = piece.parentElement.id.includes('1');
-    if (isBlackPawn && inRow1) {
-        piece.classList.remove('black-pawn');
-        piece.classList.add('black-queen');
-        return;
-    }
-}
-
-function logic() {
-    const square = this;
-    let isPiece = false;
-    if (square.lastElementChild) {
-        isPiece = square.lastElementChild.className.includes('piece');
-    }
-
-    if (isPiece) {
-        if (capture.piece2) {
-            square.removeChild(capture.piece2);
-            square.append(capture.piece1);
-        } else {
-            return
-        }
-    } else if (movement.piece) {
-        movement.square = square;
-        movement.square.append(movement.piece);
-    } else {
-        return
-    }
-
-    promotion(capture.piece1);
-    cleanNextMove();
-}
+coordinates.forEach((coordinate) => {
+    let square = document.querySelector(`#${coordinate.square}`);
+    let piece = document.createElement("span");
+    piece.classList.add("piece", `${coordinate.piece}`);
+    square.appendChild(piece);
+});
